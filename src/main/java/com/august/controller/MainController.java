5464436d99e78e0464769b80477bf7552974a6a1
August <451558608@qq.com>
2017-06-08 23:20:05 +0800

    first_commit

---
+++ src/main/java/com/august/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.august.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ModelAndView first_page(){
+		ModelAndView modelAndView = new ModelAndView("first_page");
+		
+		return modelAndView;
+	}
+}
